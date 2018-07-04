d4a5def94fc3d49fb7b9d919000a76ac76a56dd2
andrereigoto <andrereigoto@gmail.com>
2018-07-04 17:42:02 +0200

    ttx0

--- MG5_aMCatNLO/cards/HELHC/genarateCosas.sh
+++ MG5_aMCatNLO/cards/HELHC/genarateCosas.sh
@@ -1,6 +1,8 @@
-in_folder="ttx0_x0bb_build_folder"
+# bb_decay="_x0bb"
+bb_decay=""
+in_folder="ttx0${bb_decay}_build_folder"
 folder_strt="mg_pp_ttx0_cosa_"
-folder_end="_0123j_5f_x0bb"
+folder_end="_0123j${bb_decay}_5f"
 
 for i in `seq -1 0.1 1` ; do 
   cp -r ${in_folder} ${folder_strt}${i/.}${folder_end} ; 
