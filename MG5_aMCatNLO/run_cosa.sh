2a3c6c4787ade1b0c36d3f5f5b21849fe0e293ff
andrereigoto <andrereigoto@gmail.com>
2018-07-11 23:21:06 +0200

    run_cosa options

--- MG5_aMCatNLO/run_cosa.sh
+++ MG5_aMCatNLO/run_cosa.sh
@@ -1,3 +1,9 @@
-cosaVall=$1 && echo "> cosaVall=${cosaVall} "
-read tempVar
-./submit_gridpack_generation.sh 15000 15000 2nw mg_pp_ttx0_cosa_${cosaVall}_0123j_5f cards/HELHC/mg_pp_ttx0_cosa_${cosaVall}_0123j_5f 1nw
+if [[ $# -eq 0 ]]; then
+  read -ep "cosa=" cosaVall
+else
+  cosaVall=$1 
+fi
+echo "> cosaVall=${cosaVall} "
+echo "./submit_gridpack_generation.sh 15000 15000 2nw mg_pp_ttx0_cosa_${cosaVall}_0123j_5f cards/HELHC/mg_pp_ttx0_cosa_${cosaVall}_0123j_5f 2nw\n"
+read -p "Press enter to proced!!"
+./submit_gridpack_generation.sh 15000 15000 2nw mg_pp_ttx0_cosa_${cosaVall}_0123j_5f cards/HELHC/mg_pp_ttx0_cosa_${cosaVall}_0123j_5f 2nw
